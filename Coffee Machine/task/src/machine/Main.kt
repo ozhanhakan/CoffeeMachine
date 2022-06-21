14ee7a6567690993bd11c40967e9f1735700818e
Hakan Ozhan <ozhanhakan@gmail.com>
2022-06-21 21:06:08 +0300

    Stage 5/6: Keep track of the supplies

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -24,59 +24,91 @@ fun main() {
     val cappucinoMoney = 6
 
     // Menu
-    getCoffeeMachineInfo(reserveWater, reserveMilk, reserveCoffeeBean, reserveDisposableCups, hasMoney)
-    println()
-    print("Write action (buy, fill, take): ")
-    var action = readln()
-    when (action) {
-        "buy" -> {
-            when (buyMenu()) {
-                1 -> {
-                    reserveWater -= espressoWater
-                    reserveCoffeeBean -= espressoBeans
-                    reserveDisposableCups -= 1
-                    hasMoney += espressoMoney
-                }
-                2 -> {
-                    reserveWater -= latteWater
-                    reserveMilk -= latteMilk
-                    reserveCoffeeBean -= latteBeans
-                    reserveDisposableCups -= 1
-                    hasMoney += latteMoney
-                }
-                3 -> {
-                    reserveWater -= cappucinoWater
-                    reserveMilk -= cappucinoMilk
-                    reserveCoffeeBean -= cappucinoBeans
-                    reserveDisposableCups -= 1
-                    hasMoney += cappucinoMoney
-                }
-            }
+    // getCoffeeMachineInfo(reserveWater, reserveMilk, reserveCoffeeBean, reserveDisposableCups, hasMoney)
 
-        }
-        "fill" -> {
-            print("Write how many ml of water do you want to add: ")
-            reserveWater += readln().toInt()
-            print("Write how many ml of milk do you want to add: ")
-            reserveMilk += readln().toInt()
-            print("Write how many grams of coffee beans do you want to add: ")
-            reserveCoffeeBean += readln().toInt()
-            print("Write how many disposable cups of coffee do you want to add: ")
-            reserveDisposableCups += readln().toInt()
-        }
-        "take" -> {
-            println("I gave you $$hasMoney")
-            hasMoney = 0
+    while (true) {
+        println()
+        print("Write action (buy, fill, take, remaining, exit): ")
+        val action = readln()
+        println()
+        when (action) {
+            "buy" -> {
+                when (buyMenu()) {
+                    "1" -> {
+                        val notEnough = controlReserve("1", reserveWater, reserveMilk, reserveCoffeeBean)
+                        if (notEnough == "") {
+                            println("I have enough resources, making you a coffee!")
+                            reserveWater -= espressoWater
+                            reserveCoffeeBean -= espressoBeans
+                            reserveDisposableCups -= 1
+                            hasMoney += espressoMoney
+                        } else println("Sorry, not enough $notEnough!")
+                    }
+                    "2" -> {
+                        val notEnough = controlReserve("2", reserveWater, reserveMilk, reserveCoffeeBean)
+                        if (notEnough == "") {
+                            println("I have enough resources, making you a coffee!")
+                            reserveWater -= latteWater
+                            reserveMilk -= latteMilk
+                            reserveCoffeeBean -= latteBeans
+                            reserveDisposableCups -= 1
+                            hasMoney += latteMoney
+                        } else println("Sorry, not enough $notEnough!")
+                    }
+                    "3" -> {
+                        val notEnough = controlReserve("3", reserveWater, reserveMilk, reserveCoffeeBean)
+                        if (notEnough == "") {
+                            println("I have enough resources, making you a coffee!")
+                            reserveWater -= cappucinoWater
+                            reserveMilk -= cappucinoMilk
+                            reserveCoffeeBean -= cappucinoBeans
+                            reserveDisposableCups -= 1
+                            hasMoney += cappucinoMoney
+                        } else println("Sorry, not enough $notEnough!")
+                    }
+                    "back" -> continue
+                }
+                //getCoffeeMachineInfo(reserveWater, reserveMilk, reserveCoffeeBean, reserveDisposableCups, hasMoney)
+
+            }
+            "fill" -> {
+                print("Write how many ml of water do you want to add: ")
+                reserveWater += readln().toInt()
+                print("Write how many ml of milk do you want to add: ")
+                reserveMilk += readln().toInt()
+                print("Write how many grams of coffee beans do you want to add: ")
+                reserveCoffeeBean += readln().toInt()
+                print("Write how many disposable cups of coffee do you want to add: ")
+                reserveDisposableCups += readln().toInt()
+                //getCoffeeMachineInfo(reserveWater, reserveMilk, reserveCoffeeBean, reserveDisposableCups, hasMoney)
+            }
+            "take" -> {
+                println("I gave you $$hasMoney")
+                hasMoney = 0
+            }
+            "remaining" -> getCoffeeMachineInfo(
+                reserveWater,
+                reserveMilk,
+                reserveCoffeeBean,
+                reserveDisposableCups,
+                hasMoney
+            )
+            "exit" -> return
         }
     }
 
-    getCoffeeMachineInfo(reserveWater, reserveMilk, reserveCoffeeBean, reserveDisposableCups, hasMoney)
-
 }
 
-fun buyMenu(): Int {
-    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
-    return readln().toInt()
+fun buyMenu(): String {
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
+    val menu = readln()
+    return when (menu) {
+        "1" -> "1"
+        "2" -> "2"
+        "3" -> "3"
+        "back" -> "back"
+        else -> "back"
+    }
 }
 
 fun getCoffeeMachineInfo(
@@ -98,21 +130,40 @@ fun getCoffeeMachineInfo(
     )
 }
 
-fun controlReserve(needCupAmount: Int, reserveWater: Int, reserveMilk: Int, reserveCoffeeBean: Int) {
-    val water200ml = 200
-    val milk50ml = 50
-    val coffeeBean15gr = 15
-    val possibleWater = reserveWater / water200ml
-    val possibleMilk = reserveMilk / milk50ml
-    val possibleCoffeeBean = reserveCoffeeBean / coffeeBean15gr
-    val possibleAmount = minOf(possibleMilk, possibleWater, possibleCoffeeBean)
-    //val possibleMore=needCupAmount-possibleAmount
-    // val oneCupOfCoffe = water200ml + milk50ml + coffeeBean15gr
-    when {
-        possibleAmount >= 1 + needCupAmount -> println("Yes, I can make that amount of coffee (and even ${possibleAmount - needCupAmount} more than that)")
-        possibleAmount >= needCupAmount -> println("Yes, I can make that amount of coffee")
-        else -> println("No, I can make only $possibleAmount cups of coffee")
+fun controlReserve(coffeeType: String, reserveWater: Int, reserveMilk: Int, reserveCoffeeBean: Int): String {
+    val espressoWater = 250
+    val espressoBeans = 16
+
+    val latteWater = 350
+    val latteMilk = 75
+    val latteBeans = 20
+
+    val cappucinoWater = 200
+    val cappucinoMilk = 100
+    val cappucinoBeans = 12
+
+    /*var possibleWater = reserveWater
+    var possibleMilk = reserveMilk
+    var possibleCoffeeBean = reserveCoffeeBean
+    val possibleAmount = minOf(possibleMilk, possibleWater, possibleCoffeeBean)*/
+    var notEnough = ""
+    when (coffeeType) {
+        "1" -> {
+            if (espressoWater > reserveWater) notEnough = "water"
+            if (espressoBeans > reserveCoffeeBean) notEnough = "coffee beans"
+        }
+        "2" -> {
+            if (latteWater > reserveWater) notEnough = "water"
+            if (latteMilk > reserveMilk) notEnough = "milk"
+            if (latteBeans > reserveCoffeeBean) notEnough = "coffee beans"
+        }
+        "3" -> {
+            if (cappucinoWater > reserveWater) notEnough = "water"
+            if (cappucinoMilk > reserveMilk) notEnough = "milk"
+            if (cappucinoBeans > reserveCoffeeBean) notEnough = "coffee beans"
+        }
     }
+    return notEnough
 }
 
 fun getReserveCoffeeBeanFromUser(): Int {
