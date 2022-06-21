9b92cbc18dd06b3d7a8e0911f67958e8d13998a0
Hakan Ozhan <ozhanhakan@gmail.com>
2022-06-21 19:12:32 +0300

    Stage 4/6: Buy, fill, take

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -3,23 +3,99 @@ package machine
 fun main() {
     // setup coffee machine reserve
     println()
-    print("Write how many ml of water the coffee machine has: ")
-    var reserveWater = getReserveWaterFromUser()
-    print("Write how many ml of milk the coffee machine has: ")
-    var reserveMilk = getReserveMilkFromUser()
-    print("Write how many grams of coffee beans the coffee machine has: ")
-    var reserveCoffeeBean = getReserveCoffeeBeanFromUser()
-    print("Write how many cups of coffee you will need: ")
-    val needCupAmount = readln().toInt()
+    // stage 4/6 init
 
-    // Control reserve
-    controlReserve(needCupAmount, reserveWater, reserveMilk, reserveCoffeeBean)
-    // recipeCoffee(needCupAmount)
-    /*val coffeeType = "cupOfCoffee"
-    when (coffeeType) {
-        "cupOfCoffee" -> cupOfCoffeRecipe(coffeeType, needCupAmount)
-        else -> println("Find another solution")
-    }*/
+    var reserveWater = 400 // getReserveWaterFromUser()
+    var reserveMilk = 540// getReserveMilkFromUser()
+    var reserveCoffeeBean = 120//getReserveCoffeeBeanFromUser()
+    var hasMoney = 550
+    var reserveDisposableCups = 9
+
+    val espressoWater = 250
+    val espressoBeans = 16
+    val espressoMoney = 4
+    val latteWater = 350
+    val latteMilk = 75
+    val latteBeans = 20
+    val latteMoney = 7
+    val cappucinoWater = 200
+    val cappucinoMilk = 100
+    val cappucinoBeans = 12
+    val cappucinoMoney = 6
+
+    // Menu
+    getCoffeeMachineInfo(reserveWater, reserveMilk, reserveCoffeeBean, reserveDisposableCups, hasMoney)
+    println()
+    print("Write action (buy, fill, take): ")
+    var action = readln()
+    when (action) {
+        "buy" -> {
+            when (buyMenu()) {
+                1 -> {
+                    reserveWater -= espressoWater
+                    reserveCoffeeBean -= espressoBeans
+                    reserveDisposableCups -= 1
+                    hasMoney += espressoMoney
+                }
+                2 -> {
+                    reserveWater -= latteWater
+                    reserveMilk -= latteMilk
+                    reserveCoffeeBean -= latteBeans
+                    reserveDisposableCups -= 1
+                    hasMoney += latteMoney
+                }
+                3 -> {
+                    reserveWater -= cappucinoWater
+                    reserveMilk -= cappucinoMilk
+                    reserveCoffeeBean -= cappucinoBeans
+                    reserveDisposableCups -= 1
+                    hasMoney += cappucinoMoney
+                }
+            }
+
+        }
+        "fill" -> {
+            print("Write how many ml of water do you want to add: ")
+            reserveWater += readln().toInt()
+            print("Write how many ml of milk do you want to add: ")
+            reserveMilk += readln().toInt()
+            print("Write how many grams of coffee beans do you want to add: ")
+            reserveCoffeeBean += readln().toInt()
+            print("Write how many disposable cups of coffee do you want to add: ")
+            reserveDisposableCups += readln().toInt()
+        }
+        "take" -> {
+            println("I gave you $$hasMoney")
+            hasMoney = 0
+        }
+    }
+
+    getCoffeeMachineInfo(reserveWater, reserveMilk, reserveCoffeeBean, reserveDisposableCups, hasMoney)
+
+}
+
+fun buyMenu(): Int {
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+    return readln().toInt()
+}
+
+fun getCoffeeMachineInfo(
+    reserveWater: Int,
+    reserveMilk: Int,
+    reserveCoffeeBean: Int,
+    reserveDisposableCups: Int,
+    hasMoney: Int
+) {
+    println(
+        """
+        The coffee machine has:
+        $reserveWater ml of water
+        $reserveMilk ml of milk
+        $reserveCoffeeBean g of coffee beans
+        $reserveDisposableCups disposable cups
+        $${hasMoney} of money
+    """.trimIndent()
+    )
 }
 
 fun controlReserve(needCupAmount: Int, reserveWater: Int, reserveMilk: Int, reserveCoffeeBean: Int) {
@@ -64,4 +140,16 @@ fun cupOfCoffeRecipe(coffeeType: String, needCupAmount: Int) {
         ${needCupAmount * coffeeBean15gr} g of coffee beans
     """.trimIndent()
     )
+}
+
+class CoffeeMachine() {
+
+}
+
+class Coffee(water: Int, milk: Int, bean: Int, cost: Int) {
+
+}
+
+class User() {
+
 }
