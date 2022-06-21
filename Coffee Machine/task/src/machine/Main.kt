8a40e56f3474e98e77e32d6805ac5c828580444f
Hakan Ozhan <ozhanhakan@gmail.com>
2022-06-21 13:28:27 +0300

    Stage 2/6: Ingredient calculator completed

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,15 +1,27 @@
 package machine
 
 fun main() {
+    print("Write how many cups of coffee you will need: ")
+    val needCupAmount = readln().toInt()
+    // recipeCoffee(needCupAmount)
+    val coffeeType = "cupOfCoffee"
+    when (coffeeType) {
+        "cupOfCoffee" -> cupOfCoffeRecipe(coffeeType, needCupAmount)
+        else -> println("Find another solution")
+    }
+}
+
+fun cupOfCoffeRecipe(coffeeType: String, needCupAmount: Int) {
+    val water200ml = 200
+    val milk50ml = 50
+    val coffeeBean15gr = 15
+    val oneCupOfCoffe = water200ml + milk50ml + coffeeBean15gr
     println(
         """
-        Starting to make a coffee
-        Grinding coffee beans
-        Boiling water
-        Mixing boiled water with crushed coffee beans
-        Pouring coffee into the cup
-        Pouring some milk into the cup
-        Coffee is ready!
+        For $needCupAmount cups of coffee you will need:
+        ${needCupAmount * water200ml} ml of water
+        ${needCupAmount * milk50ml} ml of milk
+        ${needCupAmount * coffeeBean15gr} g of coffee beans
     """.trimIndent()
     )
 }
